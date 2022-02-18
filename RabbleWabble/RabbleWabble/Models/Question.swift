79ec05e4b145a1a249a30813a1c92d22b462378d
SChan-Kim <instufld@gmail.com>
2022-02-18 20:48:22 +0900

    feat: MVC 패턴 적용

---
+++ RabbleWabble/RabbleWabble/Models/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  RabbleWabble
+//
+//  Created by 김승찬 on 2022/02/18.
+//
+
+import Foundation
+
+public struct Question {
+    public let answer: String
+    public let hint: String?
+    public let prompt: String
+}
---
+++ RabbleWabble/RabbleWabble/Models/QuestionGroup.swift
@@ -0,0 +1,13 @@
+//
+//  QuestionGroup.swift
+//  RabbleWabble
+//
+//  Created by 김승찬 on 2022/02/18.
+//
+
+import Foundation
+
+public struct QuestionGroup {
+    public let questions: [Question]
+    public let title: String
+}
